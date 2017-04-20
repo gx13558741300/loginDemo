8edca91fe9288d8e311190849c7bb19051867fcb
gx <13558741300@163.com>
2017-04-20 17:41:29 +0800

    修改测试用例，并解决了依赖包的路径错误问题

--- test/service/UserServiceTest.java
+++ test/service/UserServiceTest.java
@@ -1,18 +1,30 @@
 package service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import domain.User;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/applicationContext.xml"})
 public class UserServiceTest {
 		
 	@Autowired
-	private static UserService userService;
+	private  UserService userService;
 	
-	public static void main(String[] args) {
+	@Test
+	public void testUserService() {
+		
+	
 //		ApplicationContext atx = 
 //				new ClassPathXmlApplicationContext("applicationContext.xml");
 //		UserService userService = (UserService) atx.getBean("userService");
